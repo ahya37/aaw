f5319ade7a4e5b2dbd945d86f054da21324fa3ec
ahya37 <ahmadyaniid37@gmail.com>
2021-09-03 14:39:15 +0700

    usermenu seeder di production

--- database/seeds/UserMenuSeeder.php
+++ database/seeds/UserMenuSeeder.php
@@ -13,8 +13,26 @@ class UserMenuSeeder extends Seeder
     public function run()
     {
        $data = [
-            ['user_id' => 68, 'menu_id' => 1],
-            ['user_id' => 68, 'menu_id' => 2]
+            ['user_id' => 52, 'menu_id' => 1],
+            ['user_id' => 52, 'menu_id' => 2],
+            ['user_id' => 119, 'menu_id' => 1],
+            ['user_id' => 119, 'menu_id' => 2],
+            ['user_id' => 228, 'menu_id' => 1],
+            ['user_id' => 228, 'menu_id' => 2],
+            ['user_id' => 198, 'menu_id' => 1],
+            ['user_id' => 198, 'menu_id' => 2],
+            ['user_id' => 53, 'menu_id' => 1],
+            ['user_id' => 53, 'menu_id' => 2],
+            ['user_id' => 100, 'menu_id' => 1],
+            ['user_id' => 100, 'menu_id' => 2],
+            ['user_id' => 249, 'menu_id' => 1],
+            ['user_id' => 249, 'menu_id' => 2],
+            ['user_id' => 145, 'menu_id' => 1],
+            ['user_id' => 145, 'menu_id' => 2],
+            ['user_id' => 35, 'menu_id' => 1],
+            ['user_id' => 35, 'menu_id' => 2],
+            ['user_id' => 251, 'menu_id' => 1],
+            ['user_id' => 251, 'menu_id' => 2]
         ];
 
         foreach($data as $row){
