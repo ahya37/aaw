19d9623ad4c3d8515e0124a5ed2a1dfff47c3236
ahya37 <ahmadyaniid37@gmail.com>
2021-09-07 17:01:48 +0700

    perbarui grafik inputer

--- resources/views/pages/admin/dashboard/index.blade.php
+++ resources/views/pages/admin/dashboard/index.blade.php
@@ -334,28 +334,7 @@
           formatter: function (x) { return x + "%"}
           });
 
-      // Job
-      Highcharts.setOptions({
-        colors: Highcharts.map(
-          Highcharts.getOptions().colors,
-          function (color) {
-            return {
-              radialGradient: {
-                cx: 0.5,
-                cy: 0.3,
-                r: 0.7,
-              },
-              stops: [
-                [0, color],
-                [1, Highcharts.color(color).brighten(-0.3).get("rgb")], // darken
-              ],
-            };
-          }
-        ),
-      });
-
-      // Build the chart
-     
+      // Build the chart     
 
       // age group
        Highcharts.chart('ageGroup', {
