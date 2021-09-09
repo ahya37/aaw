32df3a64cec928f3b83efab0dff9607d1a009392
ahya37 <ahmadyaniid37@gmail.com>
2021-09-09 16:29:03 +0700

    grafik capaian anggota perhari

--- resources/views/pages/admin/dashboard/index.blade.php
+++ resources/views/pages/admin/dashboard/index.blade.php
@@ -357,6 +357,35 @@
     $('#created_at').daterangepicker({
       startDate: start,
       endDate: end,
+      "locale": {
+        "format": "DD/MM/YYYY",
+        "separator": " - ",
+        "customRangeLabel": "Custom",
+        "daysOfWeek": [
+            "Min",
+            "Sen",
+            "Sel",
+            "Rab",
+            "Kam",
+            "Jum",
+            "Sab"
+        ],
+        "monthNames": [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "Mei",
+            "Jun",
+            "Jul",
+            "Agu",
+            "Sep",
+            "Okt",
+            "Nov",
+            "Des"
+        ],
+        "firstDay": 0
+    }
 
     },function(first, last){
       var self = this;
